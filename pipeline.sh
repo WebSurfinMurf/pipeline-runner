b85a5b6530dd31e5b751fb6482cf421d02f13770
WebSurfinMurf <websurfinmurf@gmail.com>
2025-05-23 21:37:27 -0400

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -77,7 +77,6 @@ while IFS='|' read -r REPO_KEY GIT_URL IMAGE_NAME CONTAINER_NAME PORT || [[ -n "
   docker run -d \
     --name "$CONTAINER_NAME" \
     -p "$PORT":"$PORT" \
-    -v /home/websurfinmurf/projects:/pipeline-runner/repos \
     "$FULL_IMAGE"
 
   log "✅ $REPO_KEY done"
