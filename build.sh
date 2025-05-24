5d365a9402910c1dcb772f4667c5d2a2864ac234
WebSurfinMurf <websurfinmurf@gmail.com>
2025-05-23 21:54:06 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ git pull origin main
 docker stop pipeline-runner 2>/dev/null || true
 docker rm   pipeline-runner 2>/dev/null || true
 docker build --no-cache -t websurfinmurf/pipeline-runner:latest .
-docker run -d   --name pipeline-runner   --env-file ~/pipeline.env  -v /home/websurfinmurf/projects:/pipeline-runner/repos -v /var/run/docker.sock:/var/run/docker.sock   pipeline-runner:latest
+docker run -d   --name pipeline-runner   --env-file ~/pipeline.env  -v /home/websurfinmurf/projects:/pipeline-runner/repos -v /var/run/docker.sock:/var/run/docker.sock   websurfinmurf/pipeline-runner:latest
