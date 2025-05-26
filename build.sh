64a93c732ada4472fc60dbb148f56f7f0e5c18d7
WebSurfinMurf <websurfinmurf@gmail.com>
2025-05-25 20:35:27 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ git pull origin main
 #remove --force-rm if you want to see results of the build
 docker build --no-cache -t websurfinmurf/pipeline-runner:latest .
 docker push websurfinmurf/pipeline-runner:latest
-cat pipeline.sh
+tail -2 pipeline.sh
 sleep 10
 docker run --rm -it   --env-file ~/pipeline.env   --entrypoint bash   websurfinmurf/pipeline-runner:latest
 #docker run -d   --name pipeline-runner   --env-file /home/websurfinmurf/projects/secrets/pipeline.env  -v /home/websurfinmurf/projects/pipeline-runner:/pipeline-runner -v /var/run/docker.sock:/var/run/docker.sock   websurfinmurf/pipeline-runner:latest
